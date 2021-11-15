1d0b341667a079ba36b4a7316fb1a930c8ccebe3
Focusucof <imthefirstfocus@gmail.com>
2021-11-15 10:26:26 -0500

    fixed the awful indentation

--- src/components/sections/MatchHistory.js
+++ src/components/sections/MatchHistory.js
@@ -39,46 +39,46 @@ const MatchHistory = ({
                 <div className="container-sm">
                     <div className={innerClasses}>
                         <div className="hero-content">
-                        <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
-                    <div className="match-history row" style={{height: 100, width: "100%", marginBottom: '25px'}}>
-                        <img src=" " style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/>
-                        <p className="match-score" style={{display: 'flex', flex: 1, transform: 'translate(40%, 100%)'}}>13-9</p>
-                        <p className="kda" style={{display: 'flex', flex: '1', transform: 'translate(80%, -50%)'}}>22/12/5</p>
-                        
-                        
-                    </div>
-                    <div className="match-history row" style={{height: 100, width: "100%", marginBottom: '25px'}}>
-                        <img src=" " style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/>
-                        <p className="match-score" style={{display: 'flex', flex: 1, transform: 'translate(40%, 100%)'}}>13-9</p>
-                        <p className="kda" style={{display: 'flex', flex: '1', transform: 'translate(80%, -50%)'}}>22/12/5</p>
-                        
-                        
-                    </div>
-                    <div className="match-history row" style={{height: 100, width: "100%", marginBottom: '25px'}}>
-                        <img src=" " style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/>
-                        <p className="match-score" style={{display: 'flex', flex: 1, transform: 'translate(40%, 100%)'}}>13-9</p>
-                        <p className="kda" style={{display: 'flex', flex: '1', transform: 'translate(80%, -50%)'}}>22/12/5</p>
-                        
-                        
-                    </div>
-                    <div className="match-history row" style={{height: 100, width: "100%", marginBottom: '25px'}}>
-                        <img src=" " style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/>
-                        <p className="match-score" style={{display: 'flex', flex: 1, transform: 'translate(40%, 100%)'}}>13-9</p>
-                        <p className="kda" style={{display: 'flex', flex: '1', transform: 'translate(80%, -50%)'}}>22/12/5</p>
-                        
-                        
-                    </div>
-                    <div className="match-history row" style={{height: 100, width: "100%", marginBottom: '25px'}}>
-                        <img src=" " style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/>
-                        <p className="match-score" style={{display: 'flex', flex: 1, transform: 'translate(40%, 100%)'}}>13-9</p>
-                        <p className="kda" style={{display: 'flex', flex: '1', transform: 'translate(80%, -50%)'}}>22/12/5</p>
-                        
-                        
+                            <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
+                                <div className="match-history row" style={{height: 100, width: "100%", marginBottom: '25px'}}>
+                                    <img src=" " style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/>
+                                    <p className="match-score" style={{display: 'flex', flex: 1, transform: 'translate(40%, 100%)'}}>13-9</p>
+                                    <p className="kda" style={{display: 'flex', flex: '1', transform: 'translate(80%, -50%)'}}>22/12/5</p>
+                                    
+                                    
+                                </div>
+                                <div className="match-history row" style={{height: 100, width: "100%", marginBottom: '25px'}}>
+                                    <img src=" " style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/>
+                                    <p className="match-score" style={{display: 'flex', flex: 1, transform: 'translate(40%, 100%)'}}>13-9</p>
+                                    <p className="kda" style={{display: 'flex', flex: '1', transform: 'translate(80%, -50%)'}}>22/12/5</p>
+                                    
+                                    
+                                </div>
+                                <div className="match-history row" style={{height: 100, width: "100%", marginBottom: '25px'}}>
+                                    <img src=" " style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/>
+                                    <p className="match-score" style={{display: 'flex', flex: 1, transform: 'translate(40%, 100%)'}}>13-9</p>
+                                    <p className="kda" style={{display: 'flex', flex: '1', transform: 'translate(80%, -50%)'}}>22/12/5</p>
+                                    
+                                    
+                                </div>
+                                <div className="match-history row" style={{height: 100, width: "100%", marginBottom: '25px'}}>
+                                    <img src=" " style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/>
+                                    <p className="match-score" style={{display: 'flex', flex: 1, transform: 'translate(40%, 100%)'}}>13-9</p>
+                                    <p className="kda" style={{display: 'flex', flex: '1', transform: 'translate(80%, -50%)'}}>22/12/5</p>
+                                    
+                                    
+                                </div>
+                                <div className="match-history row" style={{height: 100, width: "100%", marginBottom: '25px'}}>
+                                    <img src=" " style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/>
+                                    <p className="match-score" style={{display: 'flex', flex: 1, transform: 'translate(40%, 100%)'}}>13-9</p>
+                                    <p className="kda" style={{display: 'flex', flex: '1', transform: 'translate(80%, -50%)'}}>22/12/5</p>
+                                    
+                                    
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                </div>
-            </div>
-        </div>
 
         </section>
     );
