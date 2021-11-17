dfe2f033e553050d37881ca1959f7cd87a7d026b
Focusucof <imthefirstfocus@gmail.com>
2021-11-17 11:07:26 -0500

    loads character images

--- src/components/sections/MatchHistory.js
+++ src/components/sections/MatchHistory.js
@@ -53,7 +53,8 @@ const MatchHistory = ({
                         <div className="hero-content">
                             <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="200">
                                 <div className="match-history row" style={{height: 100, width: "100%", marginBottom: '25px'}}>
-                                    <img src=" " style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/>
+                                    {loading ? "" : <img src={"https://media.valorant-api.com/agents/" + matches[0].agent + "/displayiconsmall.png"} style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/>}
+                                    {/*<img src={"https://media.valorant-api.com/agents/" + loading ? "" : matches[0].agent + "/displayiconsmall.png"} style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/>*/}
                                     <h3 className="match-score" style={{}}>{loading ? "Loading" : JSON.stringify(matches[0].teamScore) + "-" + JSON.stringify(matches[0].enemyScore)} </h3>
                                     <p className="kda" style={{}}>22/12/5</p>
                                     
