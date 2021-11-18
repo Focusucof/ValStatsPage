7d82b3f4d6d9a9a1bac8d13360e9deac298b3490
Focusucof <imthefirstfocus@gmail.com>
2021-11-17 22:58:12 -0500

    added fading effects on match history

--- src/components/sections/MatchHistory.js
+++ src/components/sections/MatchHistory.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
 import axios from 'axios';
+import FadeIn from 'react-fade-in';
 
 const MatchHistory = ({
     className,
@@ -52,37 +53,37 @@ const MatchHistory = ({
                         <div className="hero-content">
                             <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="200">
                                 <div className="match-history row" style={{height: 100, width: "100%", marginBottom: '25px'}}>
-                                    {loading ? "" : <img src={"https://media.valorant-api.com/agents/" + matches[0].agent + "/displayiconsmall.png"} style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/>}
-                                    <h3 className="match-score" style={{}}>{loading ? "Loading" : JSON.stringify(matches[0].teamScore) + "-" + JSON.stringify(matches[0].enemyScore)} </h3>
-                                    <p className="kda" style={{}}>{loading ? "" : JSON.stringify(matches[0].kills) + "/" + JSON.stringify(matches[0].deaths) + "/" + JSON.stringify(matches[0].assists)}</p>
+                                    {loading ? "" : <FadeIn delay='150'><img src={"https://media.valorant-api.com/agents/" + matches[0].agent + "/displayiconsmall.png"} style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/></FadeIn>}
+                                    <h3 className="match-score" style={{}}>{loading ? "Loading" : <FadeIn delay='100'>{JSON.stringify(matches[0].teamScore) + "-" + JSON.stringify(matches[0].enemyScore)}</FadeIn>} </h3>
+                                    <p className="kda" style={{}}>{loading ? "" : <FadeIn delay='200'>{JSON.stringify(matches[0].kills) + "/" + JSON.stringify(matches[0].deaths) + "/" + JSON.stringify(matches[0].assists)}</FadeIn>}</p>
                                     
                                     
                                 </div>
                                 <div className="match-history row" style={{height: 100, width: "100%", marginBottom: '25px'}}>
-                                {loading ? "" : <img src={"https://media.valorant-api.com/agents/" + matches[1].agent + "/displayiconsmall.png"} style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/>}
-                                    <h3 className="match-score" style={{}}>{loading ? "Loading" : JSON.stringify(matches[1].teamScore) + "-" + JSON.stringify(matches[1].enemyScore)} </h3>
-                                    <p className="kda" style={{}}>{loading ? "" : JSON.stringify(matches[1].kills) + "/" + JSON.stringify(matches[1].deaths) + "/" + JSON.stringify(matches[1].assists)}</p>
+                                    {loading ? "" : <FadeIn delay='150'><img src={"https://media.valorant-api.com/agents/" + matches[1].agent + "/displayiconsmall.png"} style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/></FadeIn>}
+                                    <h3 className="match-score" style={{}}>{loading ? "Loading" : <FadeIn delay='100'>{JSON.stringify(matches[1].teamScore) + "-" + JSON.stringify(matches[1].enemyScore)}</FadeIn>} </h3>
+                                    <p className="kda" style={{}}>{loading ? "" : <FadeIn delay='200'>{JSON.stringify(matches[1].kills) + "/" + JSON.stringify(matches[1].deaths) + "/" + JSON.stringify(matches[1].assists)}</FadeIn>}</p>
                                     
                                     
                                 </div>
                                 <div className="match-history row" style={{height: 100, width: "100%", marginBottom: '25px'}}>
-                                {loading ? "" : <img src={"https://media.valorant-api.com/agents/" + matches[2].agent + "/displayiconsmall.png"} style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/>}
-                                    <h3 className="match-score" style={{}}>{loading ? "Loading" : JSON.stringify(matches[2].teamScore) + "-" + JSON.stringify(matches[2].enemyScore)} </h3>
-                                    <p className="kda" style={{}}>{loading ? "" : JSON.stringify(matches[2].kills) + "/" + JSON.stringify(matches[2].deaths) + "/" + JSON.stringify(matches[2].assists)}</p>
+                                    {loading ? "" : <FadeIn delay='150'><img src={"https://media.valorant-api.com/agents/" + matches[2].agent + "/displayiconsmall.png"} style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/></FadeIn>}
+                                    <h3 className="match-score" style={{}}>{loading ? "Loading" : <FadeIn delay='100'>{JSON.stringify(matches[2].teamScore) + "-" + JSON.stringify(matches[2].enemyScore)}</FadeIn>} </h3>
+                                    <p className="kda" style={{}}>{loading ? "" : <FadeIn delay='200'>{JSON.stringify(matches[2].kills) + "/" + JSON.stringify(matches[2].deaths) + "/" + JSON.stringify(matches[2].assists)}</FadeIn>}</p>
                                     
                                     
                                 </div>
                                 <div className="match-history row" style={{height: 100, width: "100%", marginBottom: '25px'}}>
-                                {loading ? "" : <img src={"https://media.valorant-api.com/agents/" + matches[3].agent + "/displayiconsmall.png"} style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/>}
-                                    <h3 className="match-score" style={{}}>{loading ? "Loading" : JSON.stringify(matches[3].teamScore) + "-" + JSON.stringify(matches[3].enemyScore)} </h3>
-                                    <p className="kda" style={{}}>{loading ? "" : JSON.stringify(matches[3].kills) + "/" + JSON.stringify(matches[3].deaths) + "/" + JSON.stringify(matches[3].assists)}</p>
+                                    {loading ? "" : <FadeIn delay='150'><img src={"https://media.valorant-api.com/agents/" + matches[3].agent + "/displayiconsmall.png"} style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/></FadeIn>}
+                                    <h3 className="match-score" style={{}}>{loading ? "Loading" : <FadeIn delay='100'>{JSON.stringify(matches[3].teamScore) + "-" + JSON.stringify(matches[3].enemyScore)}</FadeIn>} </h3>
+                                    <p className="kda" style={{}}>{loading ? "" : <FadeIn delay='200'>{JSON.stringify(matches[3].kills) + "/" + JSON.stringify(matches[3].deaths) + "/" + JSON.stringify(matches[3].assists)}</FadeIn>}</p>
                                     
                                     
                                 </div>
                                 <div className="match-history row" style={{height: 100, width: "100%", marginBottom: '25px'}}>
-                                {loading ? "" : <img src={"https://media.valorant-api.com/agents/" + matches[4].agent + "/displayiconsmall.png"} style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/>}
-                                    <h3 className="match-score" style={{}}>{loading ? "Loading" : JSON.stringify(matches[4].teamScore) + "-" + JSON.stringify(matches[4].enemyScore)} </h3>
-                                    <p className="kda" style={{}}>{loading ? "" : JSON.stringify(matches[4].kills) + "/" + JSON.stringify(matches[4].deaths) + "/" + JSON.stringify(matches[4].assists)}</p>
+                                    {loading ? "" : <FadeIn delay='150'><img src={"https://media.valorant-api.com/agents/" + matches[4].agent + "/displayiconsmall.png"} style={{height: 100, width: 100, marginLeft: '0%', display: 'flex', flex: 1}} className="column" alt=""/></FadeIn>}
+                                    <h3 className="match-score" style={{}}>{loading ? "Loading" : <FadeIn delay='100'>{JSON.stringify(matches[4].teamScore) + "-" + JSON.stringify(matches[4].enemyScore)}</FadeIn>} </h3>
+                                    <p className="kda" style={{}}>{loading ? "" : <FadeIn delay='200'>{JSON.stringify(matches[4].kills) + "/" + JSON.stringify(matches[4].deaths) + "/" + JSON.stringify(matches[4].assists)}</FadeIn>}</p>
                                     
                                     
                                 </div>
